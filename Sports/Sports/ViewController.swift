b177fff8aaabb76d9aafb97159e614cd3c63c556
Pavan Baddeputi <pavankumar.baddeputi@resideo.com>
2023-01-15 11:42:47 +0530

    Initial Project Created

---
+++ Sports/Sports/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports
+//
+//  Created by Pavan Baddeputi on 15/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
